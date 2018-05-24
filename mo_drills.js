5e1a6955d56aec12b945f210eb5b184106978dfc
Alex Winitzky <alex.winitzky@gmail.com>
2018-05-23 23:53:44 -0600

    added 6 js drills

--- mo_drills.js
+++ mo_drills.js
@@ -10,7 +10,7 @@ function pal(str) {
   }
   return true
 }
-pal('racecar')
+console.log(pal('racecar'))
 
 // alphbetical
 function alpha(str) {
@@ -19,9 +19,9 @@ function alpha(str) {
   var backToString = sortArr.join('')
   return backToString
 }
-alpha('makethisatoz')
+console.log(alpha('makethisatoz'))
 
-// capitalize
+// title case
 function caps(str) {
   words = str.split(' ')
   for (var i = 0; i < words.length; i++) {
@@ -38,7 +38,76 @@ function caps(str) {
 console.log(caps('this needs upper case'))
 
 // longest word
-
-function word(str) {
-
+function longestWord(string) {
+  str = string.split(" ")
+  var longest = 0
+  var word = null
+  str.forEach(function(str) {
+      if (longest < str.length) {
+          longest = str.length
+          word = str
+      }
+  })
+  return word;
 }
+console.log(longestWord('My name is Cornelius and it is long'))
+
+// vowel count
+function vowelCount(string) {
+  var reg = /[^A-Za-z0-9]/g
+  str = string.toLowerCase().replace(reg, '')
+  var vowels = ['a', 'e', 'i', 'o', 'u']
+  var count = 0
+  str.split('').forEach(function(letter) {
+    if (vowels.includes(letter))
+      count++
+  })
+  return count
+}
+console.log(vowelCount('Do you know how many vowels are in this sentence?'))
+
+// prime
+function prime(num) {
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num !== 1;
+}
+console.log(prime(439))
+
+// type
+function whichType(input) {
+  return typeof(input)
+}
+console.log(whichType())
+
+// penultimate low and high
+function penult() {
+  var arr = [1,2,3,4,5,6,7,8,9,10]
+  var lowHigh = []
+  var seclow = arr[1]
+  lowHigh.push(seclow)
+  var sechigh = arr.length - 1
+  lowHigh.push(sechigh)
+  return lowHigh
+}
+
+console.log(penult())
+
+// positive int factors
+function factors(n) {
+  var numFactors = [], i
+  
+  for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
+    if (n % i === 0) {
+      numFactors.push(i)
+      if (n / i !== i)
+        numFactors.push(n / i)
+   }
+   numFactors.sort(function(x, y) {
+     return x - y;})
+   
+   return numFactors
+ }
+ console.log(factors(27))
+
+ // coins
